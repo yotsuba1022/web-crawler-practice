b95db4f00ba3f83bf9efdb744d8d41d80e369335
yotsuba1022 <yotsuba1022@gmail.com>
2017-05-11 23:05:30 +0800

    Polish google_finance.py.

--- 003/google_finance.py
+++ 003/google_finance.py
@@ -1,14 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+# To query the specific stock info, add "MARKET:STOCK_ID" after the url.
+# e.g.: https://www.google.com/finance?q=[MARKET]:[STOCK_ID]
 GOOGLE_FINANCE_INFO = "https://www.google.com/finance?q="
 GOOGLE_FINANCE_HISTORY = "https://www.google.com/finance/historical?q="
 CODE_TSMC = "TPE:2330"
+FINANCE_HISTORY_START_BASE = 30
+FINANCE_HISTORY_RECORD_SIZE = 30
 
 
 def get_web_page(url, query):
-    resp = requests.get(url+query)
+    if query:
+        resp = requests.get(url+query)
+    else:
+        resp = requests.get(url)
+
     if resp.status_code != 200:
         print('Invalid url: ', resp.url)
         return None
@@ -47,8 +54,13 @@ def main():
         for k, v in stock.items():
             print(k + ":", v)
 
-    finance_history_page = get_web_page(GOOGLE_FINANCE_HISTORY, CODE_TSMC)
-    get_stock_history(finance_history_page)
+    # To fetch history data by pagination, you need to add two query params: start and num.
+    # Just like the following link:
+    # https://www.google.com/finance/historical?q=TPE:2330&start=30&num=30
+    for page in range(0, 3, 1):
+        history_url = GOOGLE_FINANCE_HISTORY + CODE_TSMC + "&start=" + str(page * FINANCE_HISTORY_START_BASE) + "&num=" + str(FINANCE_HISTORY_RECORD_SIZE)
+        finance_history_page = get_web_page(history_url, None)
+        get_stock_history(finance_history_page)
 
 
 if __name__ == '__main__':
