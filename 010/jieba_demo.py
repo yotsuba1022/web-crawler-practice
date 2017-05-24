f795c2d49d7a8eab36d67ea6832a8b012139e363
yotsuba1022 <yotsuba1022@gmail.com>
2017-05-24 23:25:26 +0800

    Implements lyrics_nlp.py with Jieba lib.

---
+++ 010/jieba_demo.py
@@ -0,0 +1,13 @@
+import jieba
+
+
+jieba.set_dictionary('../dictionary/dict.txt.big')  # 對繁體中文斷詞較準確的字典檔
+
+
+def main():
+    s = '啥小?我點的蚵仔煎裡面沒有蚵仔也沒有蛋啊!!!幹你娘!!好人你幫幫人民的啦 為基金福利會設護叫天主善的啦 聯合國把雞爆加你們聯合國 洨敬白布雞使喚唷際甘草精華雄沒醉不女人'
+    print([word for word in jieba.cut(s)])
+
+
+if __name__ == '__main__':
+    main()
---
+++ 010/lyrics_nlp.py
@@ -0,0 +1,32 @@
+import json
+import jieba
+import matplotlib.pyplot as plt
+from collections import Counter
+from wordcloud import WordCloud
+
+MAYDAY_LYRICS_LIB = 'MayDay.json'
+AIMER_ENGLISH_LIB = 'Aimer-eng.json'
+
+
+def analyze_lyrics(lyrics_file):
+    print('Analyzing %s...' % lyrics_file)
+    with open('./lyrics_lib/%s' % lyrics_file, 'r', encoding='UTF-8') as lyrics:
+        data = json.load(lyrics)
+    words = list()
+    for song in data.values():
+        words += [word for word in jieba.cut(song) if word.split() and len(word) > 1]
+
+    counter = Counter(words)
+    print(counter.most_common(10))
+
+    word_cloud = WordCloud(font_path='./fonts/NotoSansMonoCJKtc-Regular.otf').generate(' '.join(words))
+    plt.imshow(word_cloud)
+    plt.axis('off')
+    plt.show()
+    print('Analyze finished.')
+
+
+if __name__ == '__main__':
+    jieba.set_dictionary('./dictionary/dict.txt.big')
+    analyze_lyrics(MAYDAY_LYRICS_LIB)
+    analyze_lyrics(AIMER_ENGLISH_LIB)
